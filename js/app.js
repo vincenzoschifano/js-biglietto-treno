608d2a37d4775db29068bb306f3b3fcf10dc1623
vincenzoschifano <vincenzoschifano94@gmail.com>
2024-07-01 04:45:51 +0200

    ADD: Js

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+let Chilometri = prompt('inserisci i chilometri')
+Chilometri = parseInt(Chilometri)
+console.log(Chilometri)
+
+prezzoBase = 0.21
+prezzoViaggio = prezzoBase * Chilometri
+console.log(prezzoViaggio)
+
+const Eta = prompt('inserisci la tua eta')
+console.log(Eta)
+
+scontoMinorenne = prezzoViaggio * 0.2
+scontoAnziano = prezzoViaggio * 0.4
+
+
+let prezzoFinale = ''
+
+if (Eta < 18) [
+    prezzoFinale = prezzoViaggio - scontoMinorenne
+]
+else if (Eta > 65) [
+    prezzoFinale = prezzoViaggio - scontoAnziano
+]
+else [
+    prezzoFinale = prezzoViaggio
+]
+
+console.log(prezzoFinale)
+
+
+const priceElement = document.getElementById('prezzo')
+console.log(priceElement)
+priceElement.innerHTML = prezzoFinale 
+
